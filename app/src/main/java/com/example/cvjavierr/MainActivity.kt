10a77a1da73ee1e29c4708c4b99a6d55a1b8f203
javier romano <romanojavier92@gmail.com>
2021-11-28 22:23:34 -0300

    add image1

--- app/src/main/java/com/example/cvjavierr/MainActivity.kt
+++ app/src/main/java/com/example/cvjavierr/MainActivity.kt
@@ -3,16 +3,22 @@ package com.example.cvjavierr
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,11 +49,31 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     CVjavierRTheme {
+     Surface(color = MaterialTheme.colors.secondaryVariant) {
+         Column(modifier = Modifier.height(300.dp),
+             verticalArrangement = Arrangement.Top,
+             horizontalAlignment = Alignment.CenterHorizontally) {
 
-        Surface(color = MaterialTheme.colors.secondaryVariant,
-            modifier = Modifier.padding(10.dp)
-                ) {
-            Text("CV JavierR", fontSize = 30.sp , textAlign = TextAlign.Center)
+             Surface(
+                 modifier = Modifier.padding(10.dp)
+             ) {
+                 Text("CV JavierR", fontSize = 30.sp , textAlign = TextAlign.Center)
+             }
+             Surface(modifier = Modifier
+                 .size(150.dp)
+                 .padding(5.dp)
+                 , shape = CircleShape,
+                 border = BorderStroke(0.5.dp, Color.Black),
+                 elevation = 4.dp,
+                 color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+             ) {
+                 Image(painter = painterResource(id = R.drawable.perfil), contentDescription ="shorlak" ,
+                     modifier=Modifier.size(130.dp)
+                     , contentScale = ContentScale.Crop)
+             }
+         }
         }
+
+
     }
 }
